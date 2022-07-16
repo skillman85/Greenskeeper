515d8067a5878aeda768578d387e850d55a419ce
James Hillman <olegunner2222@gmail.com>
2022-07-16 15:57:48 +0100

    nav bar

--- main.js
+++ main.js
@@ -1,13 +1,8 @@
 
 
-const menuBtn = document.querySelector('.menu-button');
-let menuOpen = false;
-menuBtn.addEventListener('click', () => {
-	if (!menuOpen) {
-		menuBtn.classList.add('open');
-		menuOpen = true;
-	} else {
-		menuBtn.classList.remove('open');
-	}
-	
-});
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
