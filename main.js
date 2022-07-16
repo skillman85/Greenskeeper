8f76de2bc7632bd7f32a61132f0e7cd5ecea5890
James Hillman <skillman85@users.noreply.github.com>
2022-07-16 15:01:43 +0100

    burger

---
+++ main.js
@@ -0,0 +1,13 @@
+
+
+const menuBtn = document.querySelector('.menu-button');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+	if (!menuOpen) {
+		menuBtn.classList.add('open');
+		menuOpen = true;
+	} else {
+		menuBtn.classList.remove('open');
+	}
+	
+});
